b8923dda5912d368fc0c548fe30484fdbe9b89ac
OleksiiMyzgin <krulatuy@gmail.com>
2018-12-15 10:59:20 +0200

    Updates

---
+++ precache-manifest.0ddb0509fc66d50f7599a8371f6c9d06.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "96c35cffc0eb23695129",
+    "url": "/OleksiiMyzgin/myAppShop/static/css/main.133140bc.chunk.css"
+  },
+  {
+    "revision": "a17a419174fbcf1266aa",
+    "url": "/OleksiiMyzgin/myAppShop/static/js/1.a17a4191.chunk.js"
+  },
+  {
+    "revision": "a267c6ac4a6fcf33b18e",
+    "url": "/OleksiiMyzgin/myAppShop/static/js/runtime~main.a267c6ac.js"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "96c35cffc0eb23695129",
+    "url": "/OleksiiMyzgin/myAppShop/static/js/main.96c35cff.chunk.js"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/haymaker-webfont.e9535f20.ttf"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/OleksiiMyzgin/myAppShop/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "09714218a945a7916f964a017e216623",
+    "url": "/OleksiiMyzgin/myAppShop/index.html"
+  }
+];
